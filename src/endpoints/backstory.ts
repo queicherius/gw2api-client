65122e8fd7f52c703531365d1aef96c6dfe9e26a
Daniel O'Grady <danielogrady@gmx.de>
2023-04-23 14:20:48 +0200

    Add backstory types

--- src/endpoints/backstory.ts
+++ src/endpoints/backstory.ts
@@ -1,6 +1,34 @@
 import { AbstractEndpoint } from '../endpoint'
+import { Profession, Race } from '../types'
 
-export class BackstoryEndpoint extends AbstractEndpoint {
+/** {@link https://wiki.guildwars2.com/wiki/API:2/characters/:id/backstory} */
+interface Backstory {
+  backstory: string[]
+}
+
+/** {@link https://wiki.guildwars2.com/wiki/API:2/backstory/answers} */
+interface Answer {
+  id: string,
+  title: string,
+  description: string,
+  journal: string,
+  question: number,
+  professions: Profession[],
+  races: Race[],
+}
+
+/** {@link https://wiki.guildwars2.com/wiki/API:2/backstory/questions} */
+interface Question {
+  id: number,
+  title: string,
+  description: string,
+  answer: number[],
+  order: number,
+  races: Race[],
+  professions: Profession[]
+}
+
+export class BackstoryEndpoint extends AbstractEndpoint<Backstory> {
   answers () {
     return new AnswersEndpoint(this)
   }
@@ -10,7 +38,7 @@ export class BackstoryEndpoint extends AbstractEndpoint {
   }
 }
 
-class AnswersEndpoint extends AbstractEndpoint {
+class AnswersEndpoint extends AbstractEndpoint<Answer> {
   constructor (client) {
     super(client)
     this.url = '/v2/backstory/answers'
@@ -22,7 +50,7 @@ class AnswersEndpoint extends AbstractEndpoint {
   }
 }
 
-class QuestionsEndpoint extends AbstractEndpoint {
+class QuestionsEndpoint extends AbstractEndpoint<Question> {
   constructor (client) {
     super(client)
     this.url = '/v2/backstory/questions'
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,4 @@
 export type ItemID = number
+
+export type Race = 'Human' | 'Norn' | 'Charr' | 'Sylvari' | 'Asura'
+export type Profession = 'Guardian' | 'Revenant' | 'Warrior' | 'Engineer' | 'Ranger' | 'Thief' | 'Elementalist' | 'Mesmer' | 'Necromancer'

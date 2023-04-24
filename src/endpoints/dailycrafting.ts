3f0d34735ae112819bda0b4ae2a3c57440a21b60
Daniel O'Grady <danielogrady@gmx.de>
2023-04-24 19:36:43 +0200

    Add daily crafting endpoint

--- src/endpoints/dailycrafting.ts
+++ src/endpoints/dailycrafting.ts
@@ -1,6 +1,8 @@
 import { AbstractEndpoint } from '../endpoint'
 
-export class DailycraftingEndpoint extends AbstractEndpoint {
+type DailyCrafting = 'lump_of_mithrilium' | 'spool_of_silk_weaving_thread' | 'spool_of_thick_elonian_cord'
+
+export class DailycraftingEndpoint extends AbstractEndpoint<DailyCrafting[]> {
   constructor (client) {
     super(client)
     this.url = '/v2/dailycrafting'

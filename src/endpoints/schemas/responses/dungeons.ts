d5fd148ace67d3a2e9e90d89a818e721166c4e86
Daniel O'Grady <danielogrady@gmx.de>
2023-05-03 19:50:50 +0200

    Make dungeon types more solid

--- src/endpoints/schemas/responses/dungeons.ts
+++ src/endpoints/schemas/responses/dungeons.ts
@@ -1,6 +1,7 @@
 export namespace Schema_1970_01_01 {
+    type Dungeons =  'ascalonian_catacombs' | 'caudecus_manor' | 'twilight_arbor' | 'sorrows_embrace' | 'citadel_of_flame' | 'honor_of_the_waves' | 'crucible_of_eternity' | 'ruined_city_of_arah'
     export interface Dungeon {
-        id: string,
+        id: Dungeons,
         paths: { 
             id: string, 
             type: 'Story' | 'Explorable' 
